3c5c4ffec7c77cf280320a900d9093ad7c4f0169
Ivan <wasserleu@gmail.com>
2023-08-02 21:04:17 +0300

    Seminar 3 task1\2

--- Seminar/Seminar_3/tasks/src/main/java/ru/tasksSeminar3/task_1/T2/Origin.java
+++ Seminar/Seminar_3/tasks/src/main/java/ru/tasksSeminar3/task_1/T2/Origin.java
@@ -1,25 +1,54 @@
 package ru.tasksSeminar3.task_1.T2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Origin {
     public static void main(String[] args) {
-        Students student1 = new Students("Иванов", 5, 222, 99);
-        Students student2 = new Students("Иванова", 5, 302, 98);
-        Students student3 = new Students("Петрова", 7, 250, 97);
-        Students student4 = new Students("Сидоров", 7, 199, 89);
-        Students student5 = new Students("Аистова", 7, 299, 90);
-        List<Students> studentsList = new ArrayList<>();
-        studentsList.add(student1);
-        studentsList.add(student2);
-        studentsList.add(student3);
-        studentsList.add(student4);
-        studentsList.add(student5);
+        Students student1 = new Students("Иванов", 5, 222, List.of(99, 55, 78));
+        Students student2 = new Students("Иванова", 5, 302, List.of(91, 95, 87));
+        Students student3 = new Students("Петрова", 7, 250, List.of(88, 69, 77));
+        Students student4 = new Students("Сидоров", 7, 199, List.of(100, 59, 78));
+        Students student5 = new Students("Аистова", 7, 299, List.of(99, 95, 88));
+        List<Students> studentsList = new ArrayList<>(Arrays.asList(
+                student1, student2, student3, student4, student5
+        ));
+//        studentsList.add(student1);
+//        studentsList.add(student2);
+//        studentsList.add(student3);
+//        studentsList.add(student4);
+//        studentsList.add(student5);
+        List<Integer> grant = getListWithConditions(studentsList);
+        System.out.println("grant = " + grant);
+    }
+
+    /**
+     * @apiNote Возвращает список студентов, оканчивающихся на "ова" и имеющих чётную сумму баллов
+     * @param studentsList
+     * @return List<Integer>
+     */
+    private static List<Integer> getListWithConditions(List<Students> studentsList) {
         List<Integer> grant = new ArrayList<>();
         for (Students student : studentsList) {
-            if (student.getName().endsWith("ова") && student.getThreeSubScore() % 2 == 0) grant.add(student.getGrant());
+            if (student.getName().endsWith("ова") && evenNum(student.getThreeSubScore())) grant.add(student.getGrant());
         }
-        System.out.println("grant = " + grant);
+        return grant;
+    }
+
+    /**
+     * @apiNote Проверяет сумму элементов списка на чётность
+     * @param score
+     * @return False or True
+     */
+    private static boolean evenNum(List<Integer> score){
+        int res = 0;
+        for (Integer el : score) {
+            res += el;
+        }
+        if (res % 2 == 0) {
+            return true;
+        }
+        return false;
     }
 }
--- Seminar/Seminar_3/tasks/src/main/java/ru/tasksSeminar3/task_1/T2/Students.java
+++ Seminar/Seminar_3/tasks/src/main/java/ru/tasksSeminar3/task_1/T2/Students.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,5 +13,5 @@ public class Students {
     private String name;
     private Integer groupNum;
     private Integer grant;
-    private Integer threeSubScore;
+    private List<Integer> threeSubScore;
 }
